59da3c81a5f36b9037f360319d9295efb846b1fd
Farhod Rahimov <farhod.rahimov0055@gmail.com>
2021-08-11 14:03:48 +0300

    start creating table

--- day-01/ex07/periodic_table.py
+++ day-01/ex07/periodic_table.py
@@ -1,48 +1,91 @@
 import sys
 
-def main():
-    with open("periodic_table.txt", 'r') as f:
-        file = list()
+def create_dict_from_file(file):
+    i = 0
+    while i < len(file):
+        file[i] = file[i].split(' = ')
+        file[i][1] = file[i][1].split(', ')
+        i += 1
+
+    i = 0
+    while i < len(file):
+        k = 0
+        while k < len(file[i][1]):
+            file[i][1][k] = file[i][1][k].split(':')
+            new_dict = {file[i][1][k][0] : file[i][1][k][1]}
+            file[i][1][k] = new_dict
+            k += 1
+        i += 1
+
+    i = 0
+    while i < len(file):
+        k = 1
+        while k < len(file[i][1]):
+            file[i][1][0].update(file[i][1][k].copy())
+            k += 1
+        while len(file[i][1]) > 1:
+            file[i][1].pop(len(file[i][1]) - 1)
+        i += 1
+    return file
+
+def read_file(file_name):
+    file = list()
+    with open(file_name, 'r') as f:
         s = f.readline()
         while len(s):
             s = s.replace('\n', '')
             file.append(s)
             s = f.readline()
-        
-        i = 0
-        while i < len(file):
-            file[i] = file[i].split(' = ')
-            file[i][1] = file[i][1].split(', ')
-            i += 1
+    return file
 
-        i = 0
-        while i < len(file):
-            k = 0
-            while k < len(file[i][1]):
-                file[i][1][k] = file[i][1][k].split(':')
-                new_dict = {file[i][1][k][0] : file[i][1][k][1]}
-                file[i][1][k] = new_dict
-                k += 1
-            i += 1
+# ------------------------------------------------------------------------------------- #
 
-        i = 0
-        while i < len(file):
-            k = 1
-            while k < len(file[i][1]):
-                file[i][1][0].update(file[i][1][k].copy())
-                k += 1
-            while len(file[i][1]) > 1:
-                file[i][1].pop(len(file[i][1]) - 1)
-            i += 1
+def start_html(f):
+    text = "<!DOCTYPE html>\n"
+    text += "<html lang=\"en\">\n"
+    text += "  <head>\n"
+    text += "      <meta charset=\"UTF-8\">\n"
+    text += "          <title>Periodic table</title>\n"
+    text += "  </head>\n"
+    text += "  <body>\n"
+    f.write(text)
 
-        # print(file)
-        print(file[0])
-        print(file[0][1][0]['position'])
-        print(file[0][1][0]['number'])
+    return None
+
+def create_table(f):
+    text = "        <h1>Periodic table</h1>\n"
+    f.write(text)
+    
+    return
+
+def end_html(f):
+    text = "  </body>\n"
+    text = "</html>\n"
+    f.write(text)
+    
+    return None
+
+def create_html_file(html_name, edited_file):
+    with open(html_name, 'w') as f:
+        start_html(f)
+        create_table(f)
+        end_html(f)
+
+
+def main():
+    file = read_file("periodic_table.txt")
+    edited_file = create_dict_from_file(file)
+    create_html_file("periodic_table.html", edited_file)
 
 
 if __name__ == '__main__':
     main()
 
+
+
+# print(my_dict[0])                           # first line
+# print(my_dict[0][0])                        # name of element
+# print(my_dict[0][1][0])                      # dict
+# print(my_dict[0][1][0]['position'])         # position
 
 # /Users/btammara/django/day-01/ex07
