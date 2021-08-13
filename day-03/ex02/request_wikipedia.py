d98a2d7bf068d8f82527ce1a49c653d0941f7c8b
Farhod Rahimov <farhod.rahimov0055@gmail.com>
2021-08-13 18:26:45 +0300

    ex02 done

--- day-03/ex02/request_wikipedia.py
+++ day-03/ex02/request_wikipedia.py
@@ -1,75 +1,70 @@
-
-import sys
-import json
-
 import requests
+import json
 from dewiki import parser
+import sys
 
-# def search(search_str, url):
-#     params = {
 
-#     }
+def check_error(response, search_str):
+    if response.status_code != 200:
+        print(f'Error. Page for {search_str} does not exist. Reason: {response.reason}', file=sys.stderr)
 
-def main():
-    url = "https://en.wikipedia.org/w/api.php"
 
-    if len(sys.argv) != 2:
-        print("Error. Wrong number of arguments. Expected 1", file=sys.stderr)
-        sys.exit(1)
-    
-    search_page = sys.argv[1]
+def search(search_str, url):
     params = {
         "action": "query",
         "format": "json",
         "list": "search",
-        "srsearch": search_page
+        "srsearch": search_str
     }
-
+    
     response = requests.get(url, params)
-
-    if (response.status_code == 404):
-        print(f"Error. Page for {sys.argv[1]} in en.wikipedia.org does not exist", file=sys.stderr)
-        sys.exit(1)
-    elif (response.status_code != 200):
-        print(f"Error.", file=sys.stderr)
-        sys.exit(1)
-
+    check_error(response, search_str)
     response = response.json()
-    print(response['query']['search'][0]['title'])
-    
-    # ------------------------------------------------------ #
-    
-    # params = {
-    #     "action": "query",
-    #     "prop": "revisions",
-    #     "titles": response['query']['search'][0]['title'],
-    #     # "rvslots": "*",
-    #     "rvprop": "content",
-    #     "format": "json"
-    # }
 
+    if len(response['query']['search']) < 1:
+        print(f'Error. Page for \'{search_str}\' does not exist')
+        sys.exit(1)
+    
+    return(response['query']['search'][0]['title'])
+
+
+def get_content(page, url):
     params = {
         "action": "parse",
-        "page": response['query']['search'][0]['title'],
+        "page": page,
         "prop": "wikitext",
         "format": "json"
     }
-    
+
     response = requests.get(url, params)
+    check_error(response, page)
+    
+    return(response.json())
 
-    if (response.status_code == 404):
-        print(f"Error. Page for {sys.argv[1]} in en.wikipedia.org does not exist", file=sys.stderr)
+
+def save_text_in_file(text, file_name):
+    file_name = file_name.replace(' ', '_')
+    try:
+        with open(file_name, 'w') as f:
+            f.write(text)
+    except Exception as err:
+        print(err, file=sys.stderr)
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Error. Wrong number of arguments. Expected 1", file=sys.stderr)
         sys.exit(1)
-    elif (response.status_code != 200):
-        print(f"Error.", file=sys.stderr)
-        sys.exit(1)
+    
+    url = "https://en.wikipedia.org/w/api.php"
+    search_str = sys.argv[1]
+    
+    page_name = search(search_str, url)
+    response = get_content(page_name, url)
 
-    response = response.json()
-    # print(response['parse']['wikitext']['*'])
+    text = parser.Parser().parse_string(response['parse']['wikitext']['*'])
 
-    print(parser.Parser().parse_string(response['parse']['wikitext']['*']))
-
-    # json.dump(response.text, sys.stdout)
+    save_text_in_file(text, page_name + '.wiki')
 
 if __name__ == '__main__':
     main()
