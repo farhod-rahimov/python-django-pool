f3c6d7c1e23900109c3cd745e3cfaa18330f4578
Farhod Rahimov <farhod.rahimov0055@gmail.com>
2021-08-13 14:20:08 +0300

    ex01 done

--- day-03/ex01/my_program.py
+++ day-03/ex01/my_program.py
@@ -1,3 +1,32 @@
-import pip
+from local_lib import path as lib
 
-print("my_program print")
+def main():
+    parent_dir = lib.os.getcwd()
+    new_dir = "new_folder"
+    
+    new_dir_path = lib.os.path.join(parent_dir, new_dir)
+    new_file_path = lib.os.path.join(new_dir_path, 'new_file.txt')
+
+    msg = "This is the output of day-03 ex-01"
+    
+    try:
+        lib.os.mkdir(new_dir_path)
+    except Exception:
+        pass
+
+    try:
+        with open (new_file_path, 'w') as f:
+            f.write(msg)
+    except Exception:
+        print(f'Cannot open \'{new_file_path}\' for writing', file=lib.sys.stderr)
+    
+    try:
+        with open (new_file_path, 'r') as f:
+            print(f.read())
+    except Exception:
+        print(f'Cannot open \'{new_file_path}\' for reading', file=lib.sys.stderr)
+
+    return None
+
+if __name__ == '__main__':
+    main()
