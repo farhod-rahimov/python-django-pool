fad1857567ae9bb6c97df5108fb02b32e633233c
Rahimov Farhod <farhod.rahimov0055@gmail.com>
2021-08-14 19:50:12 +0300

    ex00 done

--- day-04/d04/ex00/views.py
+++ day-04/d04/ex00/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def main_page(request):
-    return (render(request, 'pages/index.html', locals()))
+    return (render(request, 'pages/index.html'))
