9c2fac3377bd7145f184930bd57429344ddd7c94
Rahimov Farhod <farhod.rahimov0055@gmail.com>
2021-08-16 22:29:03 +0300

    ex03 done

--- day-05/d05/ex02/views.py
+++ day-05/d05/ex02/views.py
@@ -1,17 +1,14 @@
-from django.db.models.base import Model
 from django.shortcuts import render
 from django.http import HttpResponse
 import psycopg2
-
-from .models import Movies
 
 # Create your views here.
 
 def create_table(request):
     try:
         conn = psycopg2.connect('dbname=djangotraining user=djangouser password=secret')
-        cur = conn.cursor()
-        cur.execute("CREATE TABLE ex02_movies ( \
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE ex02_movies ( \
             title varchar(64) NOT NULL UNIQUE, \
             episode_nb int PRIMARY KEY, \
             opening_crawl text NULL, \
@@ -19,7 +16,7 @@ def create_table(request):
             producer varchar(128) NOT NULL, \
             release_date date NOT NULL);")
         conn.commit()
-        cur.close()
+        cursor.close()
         conn.close()
     except Exception as err:
         return(HttpResponse(err))
@@ -36,19 +33,36 @@ def populate(request):
             {'episode_nb' : 6, 'title' : 'Return of the Jedi',       'director' : 'Richard Marquand','producer' : 'Howard G. Kazanjian, George Lucas, Rick McCallum',    'release_date' : '1983-05-25'},
             {'episode_nb' : 7, 'title' : 'The Force Awakens',        'director' : 'J. J. Abrams',    'producer' : 'Kathleen Kennedy, J. J. Abrams, Bryan Burk',          'release_date' : '2015-12-11'},
         ]
+    
+    try:
+        conn = psycopg2.connect('dbname=djangotraining user=djangouser password=secret')
+        cursor = conn.cursor()
+    except Exception as err:
+        return HttpResponse(err)
 
     for i in data:
+        cmd = f"INSERT INTO ex02_movies (title, episode_nb, director, producer, release_date) VALUES (\
+            '{i['title']}', {i['episode_nb']}, '{i['director']}', \
+            '{i['producer']}', '{i['release_date']}');"
+        
         try:
-            Movies.objects.create(episode_nb=i['episode_nb'], title=i['title'], director=i['director'],
-                        producer=i['producer'], release_date=i['release_date'])
+            cursor.execute(cmd)
             return_msg += "OK" + "<br>"
         except Exception as err:
             return_msg += str(err) + "<br>"
 
+    conn.commit()
+    cursor.close()
+    conn.close()
     return HttpResponse(return_msg)
 
 def display(request):
-    data = Movies.objects.all()
-    a = {'data': data}
+    try:
+        conn = psycopg2.connect('dbname=djangotraining user=djangouser password=secret')
+        cursor = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+        cursor.execute("select * from ex02_movies")
+        data = cursor.fetchall()
+    except Exception as err:
+        return HttpResponse("No data available")
 
     return render(request, 'ex02/display_movies.html', {'data': data})
