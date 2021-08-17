ec722b918f60935f2ba9295d00ef44022fb32346
Rahimov Farhod <farhod.rahimov0055@gmail.com>
2021-08-17 23:28:53 +0300

    trash deleted

--- day-05-intra/d05/ex02/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('init/', views.create_table),
-    path('populate/', views.populate),
-    path('display/', views.display),
-]
