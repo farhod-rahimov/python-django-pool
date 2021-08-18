0d900625a4ae1fd846599a489e59b0a69e1c7cdf
Farhod Rahimov <farhod.rahimov0055@gmail.com>
2021-08-18 18:56:30 +0300

    still on ex01

--- day-06/d06/ex01/views.py
+++ day-06/d06/ex01/views.py
@@ -1,3 +1,4 @@
+from django.http import response
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
@@ -5,6 +6,8 @@ from django.conf import settings
 import random
 
 from .forms import signUpForm, logInForm
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 
 def main(request):
@@ -20,13 +23,50 @@ def main(request):
     return response
 
 def signup(request):
+    data = {}
+
+    if request.user.is_authenticated:
+        return render(request, 'ex01/main_page.html')
+
+    if request.method == 'POST':
+        form = signUpForm(request.POST)
+        data['form'] = form
+
+        if form.is_valid():
+            if request.POST['password'] == request.POST['confirm_password']:
+                try:
+                    user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+                    user.save()
+                    login(request, user)
+                    user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+                    if user is None:
+                        return HttpResponse(f"Error. Cannot authenticate user {request.POST['username']}")
+                    login(request, user)
+                    request.COOKIES['username'] = request.POST['username']
+                    response = render(request, 'ex01/main_page.html')
+                    response.set_cookie('username', request.POST['username'], max_age=31536000) # 1 year
+                    return response
+                except Exception:
+                    data['error'] = 'Error. Username is already in use. Please choose another one'
+            else:
+                data['error'] = 'Error. Passwords are not equal'
+        else:
+            data['error'] = 'Error. Passwords are not equal'
+        
+        return render(request, 'ex01/signup.html', data)
+
     form = signUpForm
-    data = {'form' : form}
+    data['form'] = form
     return render(request, 'ex01/signup.html', data)
     return HttpResponse("signup page")
 
 def login(request):
+    data = {}
+
+    if request.method == 'POST':
+        return HttpResponse("login post method")
+    
     form = logInForm
-    data = {'form' : form}
+    data['form'] = form
     return render(request, 'ex01/login.html', data)
     return HttpResponse("login page")
